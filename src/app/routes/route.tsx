099556934571668bcd90cefce63c4929bcb03458
Agung Kurniadi <agungkurniadi99@gmail.com>
2025-07-12 14:58:16 +0700

    feat: add hero ui library

--- src/app/routes/route.tsx
+++ src/app/routes/route.tsx
@@ -1,6 +1,7 @@
 import { ErrorComponent } from '@refinedev/core';
 import { NavigateToResource } from '@refinedev/react-router';
-import { Outlet, Route, Routes } from 'react-router';
+import { HeroUIProvider } from '@heroui/system';
+import { Outlet, Route, Routes, useHref, useNavigate } from 'react-router';
 
 import {
   CategoryCreate,
@@ -11,25 +12,33 @@ import {
 import { Layout } from '@/components/layout';
 
 export function AppRoute() {
-  return (
-    <Routes>
-      <Route
-        element={
-          <Layout>
-            <Outlet />
-          </Layout>
-        }
-      >
-        <Route index element={<NavigateToResource resource="blog_posts" />} />
+  const navigate = useNavigate();
 
-        <Route path="/categories">
-          <Route index element={<CategoryList />} />
-          <Route path="create" element={<CategoryCreate />} />
-          <Route path="edit/:id" element={<CategoryEdit />} />
-          <Route path="show/:id" element={<CategoryShow />} />
+  return (
+    <HeroUIProvider
+      navigate={navigate}
+      useHref={useHref}
+      className="relative flex h-dvh w-full flex-col overflow-x-hidden bg-background font-[450] text-foreground"
+    >
+      <Routes>
+        <Route
+          element={
+            <Layout>
+              <Outlet />
+            </Layout>
+          }
+        >
+          <Route index element={<NavigateToResource resource="blog_posts" />} />
+
+          <Route path="/categories">
+            <Route index element={<CategoryList />} />
+            <Route path="create" element={<CategoryCreate />} />
+            <Route path="edit/:id" element={<CategoryEdit />} />
+            <Route path="show/:id" element={<CategoryShow />} />
+          </Route>
+          <Route path="*" element={<ErrorComponent />} />
         </Route>
-        <Route path="*" element={<ErrorComponent />} />
-      </Route>
-    </Routes>
+      </Routes>
+    </HeroUIProvider>
   );
 }
